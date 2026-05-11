36b48c949a4a5338824c5f0f637386ce1ad50667
Vasilena Yoneva <114015792+Vasillena@users.noreply.github.com>
2026-05-11 20:44:00 +0300

    Update Navbar.tsx

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -125,11 +125,11 @@ export default function Navbar(): JSX.Element {
               <div className="w-48 absolute top-[120%] -left-40 bg-white shadow-md rounded-lg p-4 text-center">
                 <h3 className="text-lg font-bold"> {t("nav.hours")}</h3>
                 <p>
-                  <span className="font-bold">{t("nav.week")}</span> 5pm - 11am
+                  <span className="font-bold">{t("nav.week")}</span> 5pm - 11pm
                 </p>
                 <p>
-                  <span className="font-bold">{t("nav.weekend")}</span> 11pm -
-                  12am
+                  <span className="font-bold">{t("nav.weekend")}</span> 12pm -
+                  11pm
                 </p>
               </div>
             )}
